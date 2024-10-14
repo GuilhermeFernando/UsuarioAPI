4f1d2ce0302f29d5d1e11a46ef0c69807b08e550
Guilherme Machado <guifermach@hotmail.com>
2024-10-14 17:05:39 -0300

    Criando usuarios

---
+++ Controller/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsuarioAPI.Controller;
+
+[ApiController]
+[Route("{controller}")]
+public class UsuarioController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Adicionausuario()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Data/Dto/CreateUsuarioDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuarioAPI.Data.Dto;
+
+public class CreateUsuarioDto 
+{
+    [Required]
+    public string Usuario { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Senha { get; set; }
+
+    [Required]
+    [Compare("Senha")]
+    public string ConfirmaSenha { get; set; }
+}
