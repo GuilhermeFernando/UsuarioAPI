f49e2393cfcfc85e16ee6de613beb1d610b3a76a
Guilherme Machado <guifermach@hotmail.com>
2024-10-16 23:30:55 -0300

    Adicionando policita de login

---
+++ Controller/AcessoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsuarioAPI.Controller;
+
+[ApiController]
+[Route("Controller")]
+public class AcessoController : ControllerBase
+{
+    [HttpGet]
+    [Authorize(Policy = "IdadeMinima")]
+    public IActionResult Get()
+    {
+        return Ok("Acesso Permtido!");
+    }
+}
